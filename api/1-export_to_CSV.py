dafd2ab992712b34a09e281815fea706a1073d2d
JRMC-PR <jesus.arafael.mendez.cruz@gmail.com>
2023-12-18 14:58:40 -0400

    Fixes pep8

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-This script fetches data from a REST API for a given employee ID, prints the employee's TODO list progress,
+This script fetches data from a REST API for a given employee
+ID, prints the employee's TODO list progress,
 and exports the data to a CSV file.
 
 Usage: python3 1-export_to-CSV.py [employee_id]
 
-The CSV file will have the following format: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+The CSV file will have the following format: "USER_ID",
+"USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
 """
 import requests
 import sys
